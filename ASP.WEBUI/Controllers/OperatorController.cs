fe4fea09396a12ae49772fb934f7a228010ab33d
Songul Esra Bingol <songul90441@ogr.duzce.edu.tr>
2024-02-15 14:20:30 +0300

    .

---
+++ ASP.WEBUI/Controllers/OperatorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.WEBUI.Controllers
+{
+	public class OperatorController : Controller
+	{
+		//Tüm operatör ekranları bu kısımda olucak
+
+		//Operator anasayfa işlemleri
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		
+	}
+}
--- ASP.WEBUI/Startup.cs
+++ ASP.WEBUI/Startup.cs
@@ -43,7 +43,7 @@ namespace ASP.WEBUI
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller=Home}/{action=Index}");
+					pattern: "{controller=Operator}/{action=Index}");
 			});
 		}
 	}
