ff87dea281b47b312f89cdf65fd00d7448b87405
Songul Esra Bingol <songul90441@ogr.duzce.edu.tr>
2024-02-17 18:19:02 +0300

    .

---
+++ ASP.WEBUI/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.WEBUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult InfoUserID()
+        {
+            //Kimlik Bilgileri
+            return View();
+        }
+        public IActionResult InfoContact()
+        {
+            //İletişim Bilgileri
+            return View();
+        }
+        public IActionResult Security()
+        {
+            return View();
+        }
+    
+    }
+}
