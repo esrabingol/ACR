a5fd7226775916915fb6e96be45a213a0f1a05c4
Songul Esra Bingol <songul90441@ogr.duzce.edu.tr>
2024-05-20 16:40:52 +0300

    Admin Account

--- ASP.WEBUI/Controllers/AdminController.cs
+++ ASP.WEBUI/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using ACR.Business.Abstract;
 using ACR.Business.Models;
 using ACR.Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
 
@@ -11,12 +12,18 @@ namespace ASP.WEBUI.Controllers
 		private IReservationService _reservationService;
 		private IRegisterService _registerService;
 		private IMachineService _machineService;
+		private IRoleService _roleService;
+		private readonly SignInManager<User> _signInManager;
+		private readonly UserManager<User> _userManager;
 
-		public AdminController(IReservationService reservationService, IRegisterService registerService, IMachineService machineService)
+		public AdminController(IReservationService reservationService, IRegisterService registerService, IMachineService machineService, IRoleService roleService, SignInManager<User> signInManager, UserManager<User> userManager)
 		{
 			_reservationService = reservationService;
 			_registerService = registerService;
 			_machineService = machineService;
+			_roleService = roleService;
+			_signInManager = signInManager;
+			_userManager = userManager;
 		}
 
 		public IActionResult Index()
@@ -67,7 +74,7 @@ namespace ASP.WEBUI.Controllers
 
 			var adReservationModel = new AdViewReservationModelDTO
 			{
-			
+
 				Results = allreservations
 			};
 			return View("Reservations", adReservationModel);
@@ -117,14 +124,67 @@ namespace ASP.WEBUI.Controllers
 			return View();
 		}
 
+		[HttpGet]
 		public IActionResult AdminLogin()
 		{
-			return View();
-		}
-		public IActionResult AdminRegister()
-		{
-			return View();
+			return View(new UserLoginModelDTO());
 		}
 
+		[HttpPost]
+		public async Task<IActionResult> AdminLogin(UserLoginModelDTO loginModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(loginModel);
+			}
+			var user = await _userManager.FindByEmailAsync(loginModel.MailAdress);
+			if (user == null)
+			{
+				TempData["ErrorMessage"] = "Mevcut Hesap Bulunamadı! Bilgilerinizi kontrol edip tekrar deneyin.";
+				return View(loginModel);
+			}
+			var result = await _signInManager.PasswordSignInAsync(user, loginModel.Password, false, false);
+
+			if (result.Succeeded)
+			{
+				var roleId = await _registerService.GetRoleIdByEmail(loginModel.MailAdress);
+				switch (roleId.Value)
+				{
+					case 3:
+						return RedirectToAction("Index", "Admin");
+
+					default:
+						return RedirectToAction("AdminLogin", "Admin");
+				}
+			}
+			else
+			{
+				TempData["ErrorMessage"] = "Kullancı Adı veya Parola Yanlış!";
+			}
+			return View(loginModel);
+		}
+
+		[HttpGet]
+		public IActionResult AdminRegister()
+		{
+			var roles = _roleService.GetRoles();
+			var userRegisterModelDTO = new UserRegisterModelDTO { Roles = roles };
+			return View(userRegisterModelDTO);
+		}
+		[HttpPost]
+		public async Task<IActionResult> AdminRegister(UserRegisterModelDTO registerModel)
+		{
+			var newAdmin = await _registerService.Add(registerModel);
+			if (newAdmin != null)
+			{
+				TempData["SuccessMessage"] = "Kaydınız Başarıyla Oluşturulmuştur!";
+				return RedirectToAction("AdminLogin");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Kaydınız oluşturulamadı eksik kısımları tamamlayınız.");
+				return View(registerModel);
+			}
+		}
 	}
 }
